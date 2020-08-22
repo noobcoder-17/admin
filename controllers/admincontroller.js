8f86fdceaaaf132d88f68fed10a07f2a516d2be8
Bhaskar Samanta <bhaskar.samanta2016@gmail.com>
2020-08-22 21:26:34 +0530

    second

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -1,3 +1,35 @@
+var firebase = require('firebase');
+
+var fire = firebase.initializeApp({
+   apiKey: "AIzaSyB0M8Qrwze4wrkRKz6NBZQ5N4ZeVMzsCCU",
+   authDomain: "groceryapp-dbf52.firebaseapp.com",
+   databaseURL: "https://groceryapp-dbf52.firebaseio.com",
+   projectId: "groceryapp-dbf52",
+   storageBucket: "groceryapp-dbf52.appspot.com",
+   messagingSenderId: "1019129350835",
+   appId: "1:1019129350835:web:3e906efe26a991f1747ec6",
+   measurementId: "G-0W3P8WERJT"
+ });
+
+ //FIREBASE ADD PRODUCT
+ const AdminAddProduct = (req, res) =>
+ {
+    var database=fire.database();
+    var productname=req.body.pname;
+    var productdes=req.body.pdes;
+    var productprice=req.body.price;
+    var producturl=req.body.purl;
+    var productcat=req.body.pcat;
+    fire.database().ref('product' + "vbnnmm,,").set({
+      productname: productname,
+      productprice: productprice,
+      productdes: productdes,
+      producturl: producturl,
+      productcat: productcat
+    });
+    res.redirect('/adminhome');
+ }
+
 //ADMIN LOGIN
 const AdminLoginCheck = ( req, res ) => 
    {
@@ -13,4 +45,4 @@ const AdminLoginCheck = ( req, res ) =>
      
     };
 
-module.exports = {AdminLoginCheck}
+module.exports = {AdminLoginCheck , AdminAddProduct}
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,14 @@ router.post('/adminlogincheck', adminController.AdminLoginCheck);
 router.get('/adminhome', function(req, res, next) {
     res.render('./adminpart/adminhome');
 });
+
+router.get('/adminaddproduct', function(req, res, next) {
+    res.render('./adminpart/productadd');
+});
+
+router.post('/AdminAddProduct' , adminController.AdminAddProduct);
+
+router.get('/adminvieworder', function(req, res, next) {
+    res.render('./adminpart/adminvieworder');
+});
 module.exports = router;
